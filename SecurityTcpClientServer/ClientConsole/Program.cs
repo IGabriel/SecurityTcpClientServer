8c5a7e0d6a850ed56436ae7308eccc57fbc92c79
Felix Chen (fchen1) <Felix.Chen.Zhuhai@quest.com>
2019-08-29 15:31:56 +0800

    Apply Log4Net, but it doesn't work...

--- SecurityTcpClientServer/ClientConsole/Program.cs
+++ SecurityTcpClientServer/ClientConsole/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using log4net;
+using Common;
 using SecurityClient;
 
 namespace ClientConsole
 {
     class Program
     {
+        private static ILog _logger;
+
         static void Main(string[] args)
         {
+            Common.LoggerFactory.Initialize();
+            _logger = Common.LoggerFactory.GetLogger(typeof(Program));
+
             int port;
             if (args != null && args.Length == 3
                 && !string.IsNullOrEmpty(args[0])
@@ -28,8 +35,13 @@ namespace ClientConsole
 
         private static void ShowUsage()
         {
-            Console.WriteLine("Connect to a SSL server.");
-            Console.WriteLine("ClientServer [server name] [port] [X509Certificate file path]");
+            _logger.Info("Connect to a SSL server.");
+            _logger.Info("ClientServer [server name] [port] [X509Certificate file path]");
+
+            // Console.WriteLine("Connect to a SSL server.");
+            // Console.WriteLine("ClientServer [server name] [port] [X509Certificate file path]");
+
+            Console.WriteLine("Yes?");
         }
     }
 }
---
+++ SecurityTcpClientServer/Common/LoggerFactory.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+
+namespace Common
+{
+    public static class LoggerFactory
+    {
+        private const string Log4netConfigFile = "log4net.config";
+
+        public static void Initialize(string log4netConfigFile = Log4netConfigFile)
+        {
+            var repository = LogManager.GetRepository(Assembly.GetExecutingAssembly());            
+            XmlConfigurator.Configure(repository, new FileInfo(log4netConfigFile));
+        }
+
+        public static ILog GetLogger(Type classType)
+        {
+            return LogManager.GetLogger(classType);
+        }
+    }
+}
