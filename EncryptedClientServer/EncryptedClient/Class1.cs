ebf59119c148ff7e4dcc358938b731bec123bb4f
FelixChen1 <felix.chen.zhuhai@quest.com>
2019-09-16 17:04:20 +0800

    Solution ready

---
+++ EncryptedClientServer/EncryptedClient/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EncryptedClient
+{
+    public class Class1
+    {
+    }
+}
---
+++ EncryptedClientServer/EncryptedServer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EncryptedServer
+{
+    public class Class1
+    {
+    }
+}
