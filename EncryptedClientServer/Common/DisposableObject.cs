fa4070d4af694b76550f1da7df92d7fbf0bc0e65
FelixChen1 <felix.chen.zhuhai@quest.com>
2019-09-16 18:43:21 +0800

    Add common classes

---
+++ EncryptedClientServer/Common/DisposableObject.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Common
+{
+    public class DisposableObject : LoggedObject, IDisposable
+    {
+        protected virtual void DisposeResource()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    DisposeResource();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ EncryptedClientServer/Common/LoggedObject.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+
+namespace Common
+{
+    public class LoggedObject
+    {
+        private ILog _logger;
+
+        protected ILog Logger
+        {
+            get
+            {
+                if (_logger == null)
+                {
+                    _logger = LoggerFactory.GetLogger(GetType());
+                }
+                return _logger;
+            }
+        }
+    }
+}
