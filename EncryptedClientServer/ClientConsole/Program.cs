5512fd9679cc930941195f354a29b59ef7dfc563
FelixChen1 <felix.chen.zhuhai@quest.com>
2019-09-17 11:57:09 +0800

    Apply log4net on ClientConsole

--- EncryptedClientServer/ClientConsole/Program.cs
+++ EncryptedClientServer/ClientConsole/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using ClientServerLibrary;
 
 namespace ClientConsole
 {
     class Program
     {
+        private static ILogger _logger;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LoggerFactory.Initialize();
+            _logger = LoggerFactory.GetLogger(typeof(Program));
+
+            if (args.Length != 2)
+            {
+                ShowUsage();
+                return;
+            }
+
+            int port;
+            if (!int.TryParse(args[1], out port))
+            {
+                ShowUsage();
+                return;
+            }
+
+            string machineName = args[0];
+            _logger.InfoFormat("Connecting server: '{0}'; port: '{1}'.", machineName, port);
+
+            using (SecurityClient client = new SecurityClient(machineName, port))
+            {
+                client.Connect();
+            }
+
         }
+
+        private static void ShowUsage()
+        {
+            _logger.Info("Connect to server.");
+            _logger.Info("ClientConsole [server name] [port]");
+        }
+
     }
 }
